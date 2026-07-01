15520451cc9548e27b66a16266fda060ea953f17
JOO <jucheonsu@gmail.com>
2026-07-01 12:59:58 +0900

    Init: Spring Boot 프로젝트 기본 구조 구성

---
+++ src/main/java/com/worksafe/backend/BackendApplication.java
@@ -0,0 +1,19 @@
+package com.worksafe.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableJpaAuditing
+@EnableScheduling
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
